5e2b5bb41dc81b7e29b2345055ef1b1ff4da2137
hitarth <hshah@bestchoiceproducts.com>
2019-08-13 23:06:59 -0700

    add images in express

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
 const forwardingAddress = "https://pwa-manifest.herokuapp.com"; // Replace this with your HTTPS Forwarding address
 
+app.use('/images', express.static(__dirname + '/icons'));
 app.get('/', (req, res) => {
     var st = {
         "name": "Best Choice Products",
@@ -23,42 +24,42 @@ app.get('/', (req, res) => {
         "start_url": "https://bestchoiceproducts.com/",
         "icons": [
           {
-            "src": "./icons/icon-72x72.png",
+            "src": "./images/icon-72x72.png",
             "sizes": "72x72",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-96x96.png",
+            "src": "./images/icon-96x96.png",
             "sizes": "96x96",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-128x128.png",
+            "src": "./images/icon-128x128.png",
             "sizes": "128x128",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-144x144.png",
+            "src": "./images/icon-144x144.png",
             "sizes": "144x144",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-152x152.png",
+            "src": "./images/icon-152x152.png",
             "sizes": "152x152",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-192x192.png",
+            "src": "./images/icon-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-384x384.png",
+            "src": "./images/icon-384x384.png",
             "sizes": "384x384",
             "type": "image/png"
           },
           {
-            "src": "./icons/icon-512x512.png",
+            "src": "./images/icon-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
           }
