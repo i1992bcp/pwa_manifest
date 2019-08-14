544b57a368a3e438c993a27abf700fcb4cc10ddc
hitarth <hshah@bestchoiceproducts.com>
2019-08-13 23:30:11 -0700

    sixth

--- index.js
+++ index.js
@@ -24,43 +24,8 @@ app.get('/', (req, res) => {
         "start_url": "https://bestchoiceproducts.com/",
         "icons": [
           {
-            "src": "/apps/manifest/images/icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-128x128.png",
-            "sizes": "128x128",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-152x152.png",
-            "sizes": "152x152",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
-          },
-          {
-            "src": "/apps/manifest/images/icon-512x512.png",
-            "sizes": "512x512",
+            "src": "images/icons/icon-64x64.png",
+            "sizes": "64x64",
             "type": "image/png"
           }
         ],
