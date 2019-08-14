3d7f03c09460cf051050c9706802357cf773e24e
hitarth <hshah@bestchoiceproducts.com>
2019-08-13 21:48:03 -0700

    final

--- index.js
+++ index.js
@@ -13,91 +13,12 @@ const scopes = 'read_products';
 const forwardingAddress = "https://pwa-manifest.herokuapp.com"; // Replace this with your HTTPS Forwarding address
 
 app.get('/', (req, res) => {
-    var st = 'Hello World!' + apiKey;
-  res.send(st);
+  const shopRequestUrl = 'https://cdn.shopify.com/s/files/1/0636/7991/t/96/assets/manifest.json?200402';
+  request.get(shopRequestUrl)
+    .then((data) => {
+        res.status(200).end(data);
+    })
 });
-
-app.get('/bcp', (req, res) => {
-    const shop = req.query.shop;
-    if (shop) {
-      const state = nonce();
-      const redirectUri = forwardingAddress + '/bcp/callback';
-      const installUrl = 'https://' + shop +'/admin/oauth/authorize?client_id=' + apiKey +'&scope=' + scopes +'&state=' + state +'&redirect_uri=' + redirectUri;
-  
-      res.cookie('state', state);
-      res.redirect(installUrl);
-    } else {
-      return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-    }
-  });
-  
-  app.get('/bcp/callback', (req, res) => {
-    const { shop, hmac, code, state } = req.query;
-    const stateCookie = cookie.parse(req.headers.cookie).state;
-  
-    if (state !== stateCookie) {
-      return res.status(403).send('Request origin cannot be verified');
-    }
-  
-    if (shop && hmac && code) {
-      // DONE: Validate request is from Shopify
-      const map = Object.assign({}, req.query);
-      delete map['signature'];
-      delete map['hmac'];
-      const message = querystring.stringify(map);
-      const providedHmac = Buffer.from(hmac, 'utf-8');
-      const generatedHash = Buffer.from(
-        crypto
-          .createHmac('sha256', apiSecret)
-          .update(message)
-          .digest('hex'),
-          'utf-8'
-        );
-      let hashEquals = false;
-  
-      try {
-        hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-      } catch (e) {
-        hashEquals = false;
-      };
-  
-      if (!hashEquals) {
-        return res.status(400).send('HMAC validation failed');
-      }
-  
-      // DONE: Exchange temporary code for a permanent access token
-      const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-      const accessTokenPayload = {
-        client_id: apiKey,
-        client_secret: apiSecret,
-        code,
-      };
-  
-      request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-      .then((accessTokenResponse) => {
-        const accessToken = accessTokenResponse.access_token;
-        // DONE: Use access token to make API call to 'shop' endpoint
-        const shopRequestUrl = 'https://' + shop + '/admin/api/2019-07/shop.json';
-        const shopRequestHeaders = {
-          'X-Shopify-Access-Token': accessToken,
-        };
-  
-        request.get(shopRequestUrl, { headers: shopRequestHeaders })
-        .then((shopResponse) => {
-          res.status(200).end(shopResponse);
-        })
-        .catch((error) => {
-          res.status(error.statusCode).send(error.error.error_description);
-        });
-      })
-      .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-      });
-  
-    } else {
-      res.status(400).send('Required parameters missing');
-    }
-  });
 
 app.use(express.static('public'))
 app.listen(process.env.PORT || 8000, () => {
